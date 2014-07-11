3643b05b1076a13e71552c8914c65d0763d2e0e4
Srujun Gupta <theununtrium@gmail.com>
2014-07-11 13:49:48 +0530

    Initial test server.

---
+++ src/com/openhearthstone/server/TestServer.java
@@ -0,0 +1,58 @@
+package com.openhearthstone.server;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+
+import java.io.IOException;
+
+public class TestServer {
+    private Server server;
+
+    TestServer() {
+        server = new Server();
+        server.addListener(new TestListener());
+
+        registerObjects();
+
+        try {
+            server.bind(6391);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.start();
+        System.out.println("Server started!");
+    }
+
+    private void registerObjects() {
+        Kryo kryo = server.getKryo();
+    }
+
+    public static void main(String args[]) {
+        new TestServer();
+    }
+
+    static class TestListener extends Listener {
+        @Override
+        public void connected(Connection connection) {
+            System.out.println("Connection: " + connection.getRemoteAddressTCP());
+        }
+
+        @Override
+        public void disconnected(Connection connection) {
+
+        }
+
+        @Override
+        public void received(Connection connection, Object object) {
+
+        }
+
+        @Override
+        public void idle(Connection connection) {
+
+        }
+    }
+}
