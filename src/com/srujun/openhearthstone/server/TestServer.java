2c169c26ede5380e3b9eedd81c64c4d87a323c30
Srujun Gupta <theununtrium@gmail.com>
2014-07-13 16:39:27 +0530

    TEST: More efforts at debugging the Request Interrupted error...

--- src/com/srujun/openhearthstone/server/TestServer.java
+++ src/com/srujun/openhearthstone/server/TestServer.java
@@ -8,9 +8,11 @@ import java.net.Socket;
 public class TestServer {
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(Integer.parseInt(System.getenv("PORT")));
+        System.out.println("Listening on: " + System.getenv("PORT"));
         try {
             while (true) {
                 Socket socket = listener.accept();
+                System.out.println("Connection from: " + listener.getInetAddress());
                 try {
                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                     out.println("HELLO!");
