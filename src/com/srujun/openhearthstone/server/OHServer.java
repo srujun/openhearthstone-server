bee808e0af48b0098ebcd898d9ef255c4c2aae87
Srujun Gupta <theununtrium@gmail.com>
2014-08-08 15:22:09 +0530

    OHServer now uses MinLog, and updated DBCollections enum.

--- src/com/srujun/openhearthstone/server/OHServer.java
+++ src/com/srujun/openhearthstone/server/OHServer.java
@@ -1,11 +1,13 @@
 package com.srujun.openhearthstone.server;
 
 import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.srujun.openhearthstone.server.listeners.ClientListener;
+import com.srujun.openhearthstone.server.listeners.CredentialsListener;
+import com.srujun.openhearthstone.server.listeners.DeckManagerListener;
 import com.srujun.openhearthstone.server.packets.KryoPackets;
 
 import java.io.IOException;
@@ -28,19 +30,20 @@ public class OHServer {
             mongoClientURI = new MongoClientURI(System.getenv("MONGOHQ_URL"));
             mongo = new MongoClient(mongoClientURI);
         } catch (UnknownHostException e) {
-            System.out.println("Error connecting to MongoHQ.");
-            System.out.println(e.getMessage());
+            Log.error("DB", "Error connecting to MongoHQ.");
+            Log.error("DB", e.getMessage());
         } finally {
             db = mongo.getDB(mongoClientURI.getDatabase());
         }
     }
 
     public static enum Collections {
-        USERS, CARDS
+        HEROES, USERS, CARDS
     }
 
     public static DBCollection getCollection(Collections col) {
         switch(col) {
+            case HEROES: return db.getCollection("hh-heroes");
             case USERS: return db.getCollection("users");
             case CARDS: return db.getCollection("hh-cards");
             default: return null;
@@ -53,26 +56,27 @@ public class OHServer {
 
         if(args.length > 0) {
             if(arguments.contains("-r")) { // Reset cards database.
-                System.out.println("[DB] Starting cards database reset.");
+                Log.info("DB", "Starting cards database reset.");
                 CardDatabaseManager cdm = new CardDatabaseManager();
                 cdm.deleteDatabase();
 
-                System.out.println("[DB] Database card count: " + OHServer.getCollection(Collections.CARDS).count());
+                Log.info("DB", "Database card count: " + OHServer.getCollection(Collections.CARDS).count());
 
                 // Now update database...
                 cdm.updateDatabase();
-                System.out.println("[DB] Database card count: " + OHServer.getCollection(Collections.CARDS).count());
+                Log.info("DB", "Database card count: " + OHServer.getCollection(Collections.CARDS).count());
             }
             if(arguments.contains("-u")) { // Update cards database.
-                System.out.println("[DB] Starting cards database update.");
+                Log.info("DB", "Starting cards database update.");
                 CardDatabaseManager cdm = new CardDatabaseManager();
                 cdm.updateDatabase();
 
-                System.out.println("[DB] Database card count: " + OHServer.getCollection(Collections.CARDS).count());
+                Log.info("DB", "Database card count: " + OHServer.getCollection(Collections.CARDS).count());
             }
         } else {
             Server server = new Server();
-            server.addListener(new ClientListener());
+            server.addListener(new CredentialsListener());
+            server.addListener(new DeckManagerListener());
             KryoPackets.registerPacketClasses(server);
 
             // Start Kryo Server
